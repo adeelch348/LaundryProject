eafc0efc3d40b18ff0814187862094e91c52e2a7
Adeel <adeel.yaseen@invozone.com>
2021-10-14 18:19:41 +0500

    changes made in seeder file

--- seeders/20211014122412-add-employee.js
+++ seeders/20211014122412-add-employee.js
@@ -2,16 +2,6 @@
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    /*
-      Add altering commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.bulkInsert('People', [{
-        name: 'John Doe',
-        isBetaMember: false
-      }], {});
-    */
     return queryInterface.bulkInsert(
       "Employees",
       [
@@ -25,12 +15,5 @@ module.exports = {
 
   down: (queryInterface, Sequelize) => {
     return queryInterface.bulkDelete("Employees", null, {});
-    /*
-      Add reverting commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.bulkDelete('People', null, {});
-    */
   },
 };
